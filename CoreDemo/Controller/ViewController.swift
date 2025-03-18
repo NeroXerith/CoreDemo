32e0c7f75d133ee9b26563e0ea865bc54e11d009
NeroXerith <astar8820@gmail.com>
2025-03-18 14:55:56 +0800

    Added a auto generated subclass for Person data  model

--- CoreDemo/Controller/ViewController.swift
+++ CoreDemo/Controller/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
---
+++ Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDemo
+//
+//  Created by Biene Bryle Sanico on 3/18/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
